383d6e142763937eb7ea28d0efc0ebd6621df3e6
zoecabrales <zoe@old.st>
2023-10-30 22:08:10 +0800

    setup backend server and db connection

---
+++ backend/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const db = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB successfully connected!");
+  } catch (err) {
+    console.log("DB connection error!");
+  }
+};
+
+module.exports = { db };
