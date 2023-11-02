c044081725c4c0bc6d0253ebcce0133b5fc8aacf
zoecabrales <zoe@old.st>
2023-11-02 16:30:54 +0800

    added expense controller, model and routes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,9 @@
 const {
+  addExpense,
+  getExpenses,
+  deleteExpense,
+} = require("../controllers/expenseController");
+const {
   addIncome,
   getIncomes,
   deleteIncome,
@@ -9,6 +14,9 @@ const nav = require("express").Router();
 nav
   .post("/add-income", addIncome)
   .get("/get-income", getIncomes)
-  .delete("/del-income/:id", deleteIncome);
+  .delete("/del-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpenses)
+  .delete("/del-expense/:id", deleteExpense);
 
 module.exports = nav;
